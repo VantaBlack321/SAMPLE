c8b4c9506441978c537423abfe8cf1202c2f05af
VantaBlack321 <76549719+VantaBlack321@users.noreply.github.com>
2026-08-07 14:10:51 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,13 +1,17 @@
 import cv2
 import numpy as np
 
-image = np.zeros((500, 500, 3), dtype=np.uint8)
+img = cv2.imread("IMG_2667.JPG")
+print(type(img)) 
+print(img.shape)  
 
-cv2.rectangle(image, (50, 50), (200, 150), (0, 255, 0), thickness=2)
+# image = np.zeros((500, 500, 3), dtype=np.uint8)
 
-cv2.circle(image, (300,100), 30, (0, 0, 225), thickness=-1)
+# cv2.rectangle(image, (50, 50), (200, 150), (0, 255, 0), thickness=2)
 
-cv2.imshow('OpenCV Example', image)
+# cv2.circle(image, (300,100), 30, (0, 0, 225), thickness=-1)
+
+# cv2.imshow('OpenCV Example', image)
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
